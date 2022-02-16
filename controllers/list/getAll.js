2c8884a7af49ce601fc7e4bc43d335a532a61942
alisaidgunes <alisaidgunes1@gmail.com>
2022-02-17 01:54:13 +0300

    Fix: naming bug in list getAll controller

--- controllers/list/getAll.js
+++ controllers/list/getAll.js
@@ -9,7 +9,7 @@ const ListService = new GetAll();
 // eslint-disable-next-line consistent-return
 const getAll = async (req, res, next) => {
   try {
-    const result = await ListService.GetTask(req.params.id);
+    const result = await ListService.GetList(req.params.id);
     if (result.success) return res.status(StatusCodes.OK).json(result);
 
     next(ApiErrorService.notFound(result.error));
