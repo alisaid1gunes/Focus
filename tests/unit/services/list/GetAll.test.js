1a3a8f1f14cac166344352614da03adbd73e9943
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-23 00:56:32 +0300

    Update list GetAll service test

--- tests/unit/services/list/GetAll.test.js
+++ tests/unit/services/list/GetAll.test.js
@@ -2,7 +2,15 @@ const { expect } = require('chai');
 
 const { GetAll } = require('../../../../src/services/list');
 
-const GetAllService = new GetAll();
+const MongooseService = require('../../../../src/services/Mongoose');
+
+const { List } = require('../../../../src/models');
+
+const MongooseServiceInstance = new MongooseService(List);
+
+const GetAllService = new GetAll(MongooseServiceInstance);
+
+const mongoose = require('mongoose');
 
 const sinon = require('sinon');
 
@@ -11,27 +19,30 @@ const hoaxer = require('hoaxer');
 describe('GetAllService Unit Tests', () => {
   describe('GetList Functionality', () => {
     it('it should successfuly return relevant users list if user is correct', async () => {
-      const id = 'id';
+      const id = new mongoose.Types.ObjectId();
 
-      const returnValue = {
-        result: [{
-          _id:  'id',
+      const userId = new mongoose.Types.ObjectId();
+
+      const taskId = new mongoose.Types.ObjectId();
+
+      const returnValue = [
+        {
+          _id: id,
           name: hoaxer.name.findName(),
-          userId:  'id',
-          tasks: [ 'id'],
-        }],
-        success: true,
-        message: 'List found.',
-      };
+          tasks: [taskId, taskId],
+        },
+      ];
 
-      const stub = sinon.stub(GetAllService, 'GetList').returns(returnValue);
+      const stub = sinon
+        .stub(MongooseServiceInstance, 'getAllWithQuery')
+        .returns(returnValue);
 
       const result = await GetAllService.GetList(id);
 
       expect(stub.calledOnce).to.be.true;
       expect(result.success).to.equal(true);
-      expect(result.message).to.equal(returnValue.message);
-      expect(result.result[0]).to.equal(returnValue.result[0]);
+      expect(result.message).to.equal('List found.');
+      expect(result.result).to.equal(returnValue);
     });
   });
 });
