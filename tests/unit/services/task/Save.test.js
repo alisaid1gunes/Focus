388d9752b1c27a2e6f2aaa185a12e56a46a3df04
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-23 00:18:11 +0300

    Update task save service test

--- tests/unit/services/task/Save.test.js
+++ tests/unit/services/task/Save.test.js
@@ -2,7 +2,15 @@ const { expect } = require('chai');
 
 const { Save } = require('../../../../src/services/task');
 
-const SaveService = new Save();
+const MongooseService = require('../../../../src/services/Mongoose');
+
+const { Task } = require('../../../../src/models');
+
+const MongooseServiceInstance = new MongooseService(Task);
+
+const SaveService = new Save(MongooseServiceInstance);
+
+const mongoose = require('mongoose');
 
 const hoaxer = require('hoaxer');
 
@@ -11,24 +19,31 @@ const sinon = require('sinon');
 describe('SaveService Unit Tests', () => {
   describe('SaveTask Functionality', () => {
     it('it should successfuly save a task if it is valid', async () => {
+      const id = new mongoose.Types.ObjectId();
+      const userId = new mongoose.Types.ObjectId();
+
       const stubValue = {
         name: hoaxer.name.findName(),
-        userId: 'id',
+        userId,
         done: true,
       };
 
       const returnValue = {
-        success: true,
-        message: 'Task saved.',
+        _id: id,
+        name: hoaxer.name.findName(),
+        userId,
+        done: true,
       };
 
-      const stub = sinon.stub(SaveService, 'SaveTask').returns(returnValue);
+      const stub = sinon
+        .stub(MongooseServiceInstance, 'save')
+        .returns(returnValue);
 
       const result = await SaveService.SaveTask(stubValue);
 
       expect(stub.calledOnce).to.be.true;
       expect(result.success).to.equal(true);
-      expect(result.message).to.equal(returnValue.message);
+      expect(result.message).to.equal('Task saved.');
     });
   });
 });
