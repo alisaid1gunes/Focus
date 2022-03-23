86da1c660beaa68f39a94b2f66c97980f341c82b
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-23 13:05:19 +0300

    Update auth/ForgetPassword service test

--- tests/unit/services/auth/ForgetPassword.test.js
+++ tests/unit/services/auth/ForgetPassword.test.js
@@ -2,7 +2,17 @@ const { expect } = require('chai');
 
 const { ForgetPassword } = require('../../../../src/services/auth');
 
-const ForgetPasswordService = new ForgetPassword();
+const MongooseService = require('../../../../src/services/Mongoose');
+
+const { User } = require('../../../../src/models');
+
+const MongooseServiceInstance = new MongooseService(User);
+
+const ForgetPasswordService = new ForgetPassword(MongooseServiceInstance);
+
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcryptjs');
 
 const hoaxer = require('hoaxer');
 
@@ -11,24 +21,44 @@ const sinon = require('sinon');
 describe('ForgetPassword Service Unit Tests', () => {
   describe('ForgetPassword Functionality', () => {
     it('it should successfuly start user forgot password process if user is in db', async () => {
+      const id = new mongoose.Types.ObjectId();
       const stubValue = {
-        id: 'id',
+        id,
       };
 
       const returnValue = {
-        success: true,
-        message: 'Verification code sent your email to change your password',
+        _id: id,
+        username: hoaxer.internet.userName(),
+        password: hoaxer.internet.password(),
+        email: hoaxer.internet.email(),
+        verification: {
+          isVerified: true,
+          code: 2561,
+          expireDate: hoaxer.date.future(),
+        },
+        activation: {
+          isActivated: true,
+          code: 2561,
+          expireDate: hoaxer.date.future(),
+        },
       };
 
-      const stub = sinon
-        .stub(ForgetPasswordService, 'ForgetPassword')
+      const getStub = sinon
+        .stub(MongooseServiceInstance, 'get')
+        .returns(returnValue);
+
+      const updateStub = sinon
+        .stub(MongooseServiceInstance, 'update')
         .returns(returnValue);
 
       const result = await ForgetPasswordService.ForgetPassword(stubValue);
 
-      expect(stub.calledOnce).to.be.true;
+      expect(getStub.calledOnce).to.be.true;
+      expect(updateStub.calledOnce).to.be.true;
       expect(result.success).to.equal(true);
-      expect(result.message).to.equal(returnValue.message);
+      expect(result.message).to.equal(
+        'Verification code sent your email to change your password'
+      );
     });
   });
 });
