4ba32c92be359f31b17deb3f87068a096b615d44
alisaidgunes <alisaidgunes1@gmail.com>
2022-03-23 21:14:55 +0300

    Update auht/Register service test

--- tests/unit/services/auth/Register.test.js
+++ tests/unit/services/auth/Register.test.js
@@ -4,30 +4,70 @@ const hoaxer = require('hoaxer');
 
 const { Register } = require('../../../../src/services/auth');
 
-const RegisterService = new Register();
+const { User } = require('../../../../src/models');
+
+const MongooseService = require('../../../../src/services/Mongoose');
+
+const MongooseServiceInstance = new MongooseService(User);
+
+const RegisterService = new Register(MongooseServiceInstance);
+
+const mongoose = require('mongoose');
 
 const sinon = require('sinon');
+
+const { generateToken } = require('../../../../src/utils/tokenGenerator');
+
+const {
+  ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET,
+} = require('../../../../src/config/config');
 
 describe('Register Service Unit Tests', () => {
   describe('RegisterUser Functionality', () => {
     it('it should successfuly add a new user if parameters are valid ', async () => {
+      const id = new mongoose.Types.ObjectId();
       const stubValue = {
-        email: hoaxer.internet.email(),
-        password: hoaxer.internet.password(),
-        username: hoaxer.name.findName(),
+        body: {
+          email: hoaxer.internet.email(),
+          password: '123456789',
+          username: 'alisaid123',
+        },
+        file: {
+          path: hoaxer.image.imageUrl(),
+        },
       };
 
-      const stub = sinon.stub(RegisterService, 'RegisterUser').returns({
-        user: stubValue,
-        username: stubValue.username,
-        success: true,
-        message: 'User created',
-      });
+      const saveReturn = {
+        _id: id,
+        username: hoaxer.internet.userName(),
+        password: '123456789',
+        email: hoaxer.internet.email(),
+        verification: {
+          isVerified: true,
+          code: 2561,
+          expireDate: hoaxer.date.future(),
+        },
+        activation: {
+          isActivated: true,
+          code: 2561,
+          expireDate: hoaxer.date.future(),
+        },
+        profileUrl: hoaxer.internet.avatar(),
+      };
+      const getStub = sinon.stub(MongooseServiceInstance, 'get').returns(null);
+
+      const saveStub = sinon
+        .stub(MongooseServiceInstance, 'save')
+        .returns(saveReturn);
 
       const result = await RegisterService.RegisterUser(stubValue);
-      expect(stub.calledOnce).to.be.true;
-      expect(result.user.email).to.equal(stubValue.email);
-      expect(result.username).to.equal(stubValue.username);
+      console.log(result);
+      expect(getStub.calledOnce).to.be.true;
+      expect(saveStub.calledOnce).to.be.true;
+      expect(result.user).to.equal(saveReturn);
+      expect(result.success).to.equal(true);
+      expect(result.message).to.equal('User created');
     });
   });
 });
